6eca837ae73cd4377b4242f2a41cbf4ddb56a999
Krystal Guzman <kryguzman@gmail.com>
2020-01-29 08:19:05 -0800

    completed with stretch

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+import Form from "./components/Form";
+import Members from "./components/Members";
+
 function App() {
+  const [members, setMembers] = useState([]);
+
+  const submitMember = member => setMembers([...members, member]);
+
+  const update = updatedMember =>
+    setMembers([
+      ...members.map(member => {
+        if (member.id === updatedMember.id) {
+          return updatedMember;
+        }
+        return member;
+      })
+    ]);
+    console.log(`members`, members);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Form add={submitMember} />
+        {members.map((member, i) => (
+          <Members member={member} key={i} update={update} />
+        ))}
     </div>
   );
 }
---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,91 @@
+import React, {useState} from "react";
+import styled from "styled-components";
+
+const Header = styled.form`
+    width: 100%;
+    border: 1px solid black;
+    padding-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+    position: -webkit-sticky;
+    position: sticky;
+    top: 0;
+`;
+
+const Child = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: space-evenly;
+`;
+
+const Button = styled.button`
+    border-radius: 20px;
+    &:hover{
+        background: lightgrey;
+        border-radius: 20px;
+        border: 1px solid grey;
+    }
+`;
+
+const Form = (props) => {
+    // console.log(`This is props from the Form.js`, props);
+    const [input, setInput] = useState({
+        name: "",
+        email: "",
+        role: "",
+        id: null
+    });
+    
+    const inputHandler = event => {
+        console.log("target name", event.target.name);
+        console.log("value", event.target.value);
+        setInput({...input, [event.target.name]: event.target.value });
+    };
+
+    const submitHandler = event => {
+        event.preventDefault();
+        props.add({...input, id: Math.random() })
+        setInput({ name: "", email: "", role: "" });
+    }
+    console.log("props", props);
+    console.log("member", input);
+
+    return (
+        <Header onSubmit={submitHandler}>
+            <h1>My Team</h1>
+            <Child>
+                <label htmlFor="name">Name: {""}
+                    <input 
+                        id= "name"
+                        type= "text"
+                        name= "name"
+                        onChange= {inputHandler}
+                        value={input.name}
+                    />
+                </label>
+                <label htmlFor="email">Email: {""}
+                    <input 
+                        id= "email"
+                        type= "text"
+                        name= "email"
+                        onChange= {inputHandler}
+                        value={input.email}
+                    />
+                </label>
+                <label htmlFor="role">Role: {""}
+                    <input 
+                        id= "role"
+                        type= "text"
+                        name= "role"
+                        onChange= {inputHandler}
+                        value={input.role}
+                    />
+                </label>
+                <Button type="submit">Add a team member</Button>
+            </Child>
+        </Header>
+    );
+}
+
+
+export default Form;
